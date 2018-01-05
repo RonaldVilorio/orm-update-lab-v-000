2d0439528314a584e1edec3ce215767608f226f6
ronald vilorio <soccer23415@gmail.com>
2018-01-05 18:19:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,15 +38,10 @@ class Student
       DB[:conn].execute(sql,@name,@grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-
   end
-  # def self.new_from_db(row)
-  #   new_student = self.new
-  #   new_student.id = row[0]
-  #   new_student.name = row[1]
-  #   new_student.grade = row[2]
-  #   new_student
-  # end
+  def self.update
+  end
+
 
 
 end
