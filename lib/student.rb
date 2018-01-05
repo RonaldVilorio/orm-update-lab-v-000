92d4613454e4127d96ab6b2b4328d61cce9ca534
ronald vilorio <soccer23415@gmail.com>
2018-01-05 18:00:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :name,:grade
-  def initialize
+  def initialize(name=nil,grade=nil)
+    @name = name
+    @grade = grade
+
   end
 
 
